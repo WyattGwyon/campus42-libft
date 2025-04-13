65ae2304cb865070416a223f2369cb7908e5db9b
Clayton Louden <clouden@student.42madrid.com>
2025-04-13 19:06:50 +0200

    successfully linked memset and bzero to test bzero

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,26 +6,39 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 13:19:40 by clouden           #+#    #+#             */
-/*   Updated: 2025/04/12 14:56:32 by clouden          ###   ########.fr       */
+/*   Updated: 2025/04/13 19:05:19 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include <stddef.h>
-#include <"libft.h">
+#include <libft.h>
 
 void ft_bzero(void *s, size_t n)
 {
-	return (ft_memset(s, '\0', n));	
+	ft_memset(s, '\0', n);	
 }
-*/
 
+/*
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
+	char ft_array[5] = "Hola";
 	char array[5] = "Hola";
 	int n = 2;
+
+	printf("bzero\n");
+	printf("before array[0]: %c\n", ft_array[0]);
+	printf("before array[1]: %c\n", ft_array[1]);
+	printf("before array[2]: %c\n", ft_array[2]);
+	printf("before array[3]: %d\n", ft_array[3]);
+	ft_bzero(ft_array, n);	
+	printf("after array[0]: %c\n", ft_array[0]);
+	printf("after array[1]: %c\n", ft_array[1]);
+	printf("after array[2]: %c\n", ft_array[2]);
+	printf("after array[3]: %d\n", ft_array[3]);	
+
 
 	printf("bzero\n");
 	printf("before array[0]: %c\n", array[0]);
@@ -35,7 +48,8 @@ int main(void)
 	bzero(array, n);	
 	printf("after array[0]: %c\n", array[0]);
 	printf("after array[1]: %c\n", array[1]);
-	printf("before array[2]: %c\n", array[2]);
-	printf("before array[3]: %d\n", array[3]);
+	printf("after array[2]: %c\n", array[2]);
+	printf("after array[3]: %d\n", array[3]);
 	return (0);	
 }
+*/
