17478c1569531f773e67eb793739da883c940455
Clayton Louden <clouden@student.42madrid.com>
2025-04-15 19:34:34 +0200

    still working on memmove.c

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,33 +6,50 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 13:05:33 by clouden           #+#    #+#             */
-/*   Updated: 2025/04/14 13:32:50 by clouden          ###   ########.fr       */
+/*   Updated: 2025/04/15 19:19:24 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	void	*buffer;
+
+	
+}
 
 #include <stdio.h>
 #include <stddef.h>
 
 int	main(void)
 {
-	int array[] = {1,2,4,3,5,6,};
-	int *a_1 = &array[2];
-	int *a_2 = &array[5];
+	int array[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	int *src = &array[2];
+	int *dest = &array[5];
 	int i = 0;
+	int n = 3;
 
-	while (i < sizeof(a_1))
+	printf("size of src: %lu\n", sizeof(src));
+	while (i < sizeof(src))
 	{
-		printf("%d\n",a_1[i]);
+		printf("%d\n",src[i]);
 		i++;
 	}
-	printf("\n");	
+	printf("\n");
+	printf("size of dest: %lu\n", sizeof(dest));	
 	i = 0;
-	while (i < sizeof(a_2))
+	while (i < sizeof(dest))
 	{
-		printf("%d\n",a_2[i]);
+		printf("%d\n",dest[i]);
+		i++;
+	}
+	printf("\n");
+	memmove(dest, src, n);
+	i = 0;
+	while (i < )
+	{
+		printf("%d\n",array[i]);
 		i++;
 	}
 	return (0);
