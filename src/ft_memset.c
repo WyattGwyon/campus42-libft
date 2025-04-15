684110b3a9ebc8a0e18bff24f18a6dda22dd3a95
Clayton Louden <clouden@student.42madrid.com>
2025-04-15 14:09:03 +0200

    optimized 10x, tested and approved ft_memset

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 18:30:14 by clouden           #+#    #+#             */
-/*   Updated: 2025/04/14 18:57:15 by clouden          ###   ########.fr       */
+/*   Updated: 2025/04/15 14:00:13 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,42 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	int	i;
-	unsigned char *bytes;
-	unsigned char b;
+	unsigned long long	pattern;
+	int					blocks;
+	int					remaining;
+	int					i;
 
+	blocks = n / 8;
+	remaining = n % 8;
+	pattern = (unsigned char)c;
+	pattern |= pattern << 8;
+	pattern |= pattern << 16;
+	pattern |= pattern << 32;
 	i = 0;
-	bytes = (unsigned char *)s;
-	b = (unsigned char)c;
-	while (i + 8 < n)
+	while (i < blocks)
 	{
-		bytes[i] = \
-		bytes[i + 1] = \
-		bytes[i + 2] = \
-		bytes[i + 3] = \
-		bytes[i + 4] = \
-		bytes[i + 5] = \
-		bytes[i + 6] = \
-		bytes[i + 7] = b;
-		i += 8;
+		((unsigned long long *)s)[i] = pattern;
+		i++;
 	}
-	while (i < n)
+	i = 0;
+	while (i < remaining)
 	{
-		bytes[i] = b;
+		((unsigned char *)s)[(blocks * 8) + i] = (unsigned char)c;
 		i++;
 	}
 	return ((void *)s);
 }
 
-
+/*
 #include <string.h>
 #include <stdio.h>
 
 int main(void)
 {
-	int ft_array[] = {2147483647, 260, 2, 40000, 3000000, 2};
-	int array[] = {2147483647, 260, 2, 40000, 3000000, 2};
+	int ft_array[] = {2147483647, 260, 2, 40000, 3000000, 2, 70000, 800000};
+	int array[] = {2147483647, 260, 2, 40000, 3000000, 2, 70000, 800000};
 	int c = 0;
-	int n = 17;
+	int n = 21;
 	
 	printf("ft_memset\n");
 	printf("antes array[0]: %d\n", ft_array[0]);
@@ -58,12 +57,18 @@ int main(void)
 	printf("antes array[2]: %d\n", ft_array[2]);
 	printf("antes array[3]: %d\n", ft_array[3]);
 	printf("antes array[4]: %d\n", ft_array[4]);
+	printf("antes array[5]: %d\n", ft_array[5]);
+	printf("antes array[6]: %d\n", ft_array[6]);
+	printf("antes array[7]: %d\n", ft_array[7]);
 	ft_memset(ft_array, c, n);
 	printf("despues array[0]: %d\n", ft_array[0]);
 	printf("despues array[1]: %d\n", ft_array[1]);
 	printf("despues array[2]: %d\n", ft_array[2]);
 	printf("despues array[3]: %d\n", ft_array[3]);
-	printf("despues array[4]: %d\n\n", ft_array[4]);
+	printf("despues array[4]: %d\n", ft_array[4]);
+	printf("despues array[5]: %d\n", ft_array[5]);
+	printf("despues array[6]: %d\n", ft_array[6]);
+	printf("despues array[7]: %d\n\n", ft_array[7]);
 
 	printf("memset\n");
 	printf("antes array[0]: %d\n", array[0]);
@@ -71,14 +76,20 @@ int main(void)
 	printf("antes array[2]: %d\n", array[2]);
 	printf("antes array[3]: %d\n", array[3]);
 	printf("antes array[4]: %d\n", array[4]);
+	printf("antes array[5]: %d\n", array[5]);
+	printf("antes array[6]: %d\n", array[6]);
+	printf("antes array[7]: %d\n", array[7]);
 	memset(array, c, n);
 	printf("despues array[0]: %d\n", array[0]);
 	printf("despues array[1]: %d\n", array[1]);
 	printf("despues array[2]: %d\n", array[2]);
 	printf("despues array[3]: %d\n", array[3]);
 	printf("despues array[4]: %d\n", array[4]);
+	printf("despues array[5]: %d\n", array[5]);
+	printf("despues array[6]: %d\n", array[6]);
+	printf("despues array[7]: %d\n", array[7]);
 
 
 	return (0);
 }
-
+*/
