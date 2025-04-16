0abce713d49edfbe466e10d09fd84b567b9be6ca
42 Madrid Student <42MadridStudent@Admins-MacBook-Pro.local>
2025-04-16 23:43:25 +0200

    memmove tested and aapporved

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,51 +6,50 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 13:05:33 by clouden           #+#    #+#             */
-/*   Updated: 2025/04/15 19:19:24 by clouden          ###   ########.fr       */
+/*   Updated: 2025/04/16 23:42:30 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-void	*memmove(void *dest, const void *src, size_t n)
-{
-	void	*buffer;
-
-	
-}
-
-#include <stdio.h>
 #include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+    int     i;
+
+    i = 0;
+    if (dest > src)
+        while (i <= n)
+        {
+            ((unsigned char *)dest)[n - i] = ((unsigned char *)src)[n - i];
+            i++;
+        }
+    else
+        while (i < n)
+        {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+    return ((void *)dest);
+}
+/*
+#include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	int array[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-	int *src = &array[2];
-	int *dest = &array[5];
-	int i = 0;
-	int n = 3;
+    int ft_src[7] = {1,20,300,4000,50000,600000,7000000};
+    int *ft_dest = ft_src + 2;
+    int src[7] = {1,20,300,4000,50000,600000,7000000};
+    int *dest = src + 2;
+    int i;
 
-	printf("size of src: %lu\n", sizeof(src));
-	while (i < sizeof(src))
-	{
-		printf("%d\n",src[i]);
-		i++;
-	}
-	printf("\n");
-	printf("size of dest: %lu\n", sizeof(dest));	
-	i = 0;
-	while (i < sizeof(dest))
-	{
-		printf("%d\n",dest[i]);
-		i++;
-	}
-	printf("\n");
-	memmove(dest, src, n);
-	i = 0;
-	while (i < )
-	{
-		printf("%d\n",array[i]);
-		i++;
-	}
-	return (0);
+    ft_memmove(ft_dest, ft_src, sizeof(int) * 7);
+    memmove(dest, src, sizeof(int) * 7);
+    for (i = 0; i < 7; i++)
+    {
+        printf("ft_dest[%d]: %d\n", i, ft_dest[i]);
+        printf("dest[%d]:    %d\n", i, dest[i]);
+    }
+    return (0);
 }
+*/
