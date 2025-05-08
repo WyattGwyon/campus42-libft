ae7b696d3bbe30f9e786dcbaf8ecac5f0727dd2c
Clayton Louden <clouden@student.42madrid.com>
2025-05-08 19:23:06 +0200

    strsplit works but needs to be refactored into compenent fuctions

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:57:14 by clouden           #+#    #+#             */
-/*   Updated: 2025/05/06 12:26:18 by clouden          ###   ########.fr       */
+/*   Updated: 2025/05/08 14:01:47 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@ void *ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
 char *ft_strchr(const char *s, int c);
 void *ft_calloc(size_t count, size_t size);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+void *ft_memset(void *s, int c, size_t n);
+char *ft_strdup(const char *s);
 
 #endif
--- src/ft_strsplit.c
+++ src/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 19:23:22 by clouden           #+#    #+#             */
-/*   Updated: 2025/05/07 19:41:32 by clouden          ###   ########.fr       */
+/*   Updated: 2025/05/08 19:21:31 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,40 @@ static int ft_cntsplits(char const *s, char dlm, size_t len)
 				i++;
 			}
 		}
-		i++;
+		else if (i + 2 == len && cnt == 0)
+			return (cnt = 2);
+		else
+			i++;
 	}
 	return (cnt + 1);
 }
 
-char **ft_split(char const *s, char c)
+char **ft_strsplit(char const *s, char c)
 {
     char **array;
     char *buff;
-    char *dest;
-    int ai:
+    int ai;
     int bi;
 	int len;
 	int strcnt;
     int b_len;
 
-    i = 0;
+    ai = 0;
+	bi = 0; 
 	len = ft_strlen(s);
     strcnt = ft_cntsplits(s, c, len);
+	if (strcnt == 1)
+	{
+		array[ai] = NULL;
+		return (array);
+	}
+	else if (strcnt == 2)
+	{
+		array[ai] = (char *)s;
+		ai++;
+		array[ai] = NULL;
+		return (array);
+	}	
 	array = (char **)ft_calloc(strcnt + 1, sizeof(char *));
     buff = (char *)ft_calloc(1, len);
     if (array == NULL)
@@ -62,12 +77,9 @@ char **ft_split(char const *s, char c)
         else if (s[0] == c && *buff != 0)
         {
             buff[bi] = '\0';
-            b_len = ft_strlen(buff);
-            dest = (char *)ft_calloc(1, b_len);
-            ft_strlcpy(dest, buff, b_len);
-            array[ai] = &dest;
-            free(dest);
-            memset(buff, 0, len);
+            b_len = ft_strlen(buff) + 1;
+			array[ai] = ft_strdup(buff);
+            ft_memset(buff, 0, len);
             s++;
             bi = 0;
             ai++;
@@ -88,12 +100,38 @@ char **ft_split(char const *s, char c)
 int main(void)
 {
     const char *s = ",,,Hello,,World, Goodbye,,";
-	const char *s1 = "Hello,World, Goodbye";
+	const char *s1 = ",,,,,,";
+	const char *s2 = "Hello World";
   	char c = ',';  
 	size_t len = ft_strlen(s);
 	size_t len1 = ft_strlen(s1);
+	char **array;
+	
 	printf("original: %s, num of strings: %d\n", s, ft_cntsplits(s, c, len));
 	printf("original: %s, num of strings: %d\n", s1, ft_cntsplits(s1, c, len));
 
-    return (0);
+    printf("original: %s\narray of strs: ", s);
+	array = ft_strsplit(s, c);
+	for (int i = 0; array[i] != NULL; i++) 
+	
+    	printf("'%s' , ", array[i]);
+		
+	printf("\n----\n");
+	
+    printf("original: %s\narray of strs: ", s1);
+	array = ft_strsplit(s1, c);
+	for (int i = 0; array[i] != NULL; i++) 
+	
+    	printf("'%s' , ", array[i]);
+		
+	printf("\n----\n");
+	
+    printf("original: %s\narray of strs: ", s2);
+	array = ft_strsplit(s2, c);
+	for (int i = 0; array[i] != NULL; i++) 
+	
+    	printf("'%s' , ", array[i]);
+		
+	printf("\n----\n");
+	return (0);
 }
