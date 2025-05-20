da3b1646bc664cfbf7879bbfbe445d01ddad57f5
Clayton Louden <clouden@student.42madrid.com>
2025-05-20 14:07:12 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:57:14 by clouden           #+#    #+#             */
-/*   Updated: 2025/05/20 12:42:45 by clouden          ###   ########.fr       */
+/*   Updated: 2025/05/20 14:06:42 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@ int ft_atoi( const char* str );
 void ft_bzero(void *s, size_t n);
 void *ft_calloc(size_t count, size_t size);
 int ft_isalnum(int c);
-int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+void *ft_memset(void *s, int c, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+
 
 #endif
